85dc5edd7eff223bd3dfbf0c469bade00c4c6426
Vidyullata Patil <vidyullata.patil@tcs.com>
2021-02-22 00:21:48 +0530

    changed base url

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/api', router);
+app.use('/', router);
 
 router.post('/login' ,async (req,res) => {
     const response = await loginUser(req);
